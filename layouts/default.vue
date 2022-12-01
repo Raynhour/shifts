2a99db6571159b46e3de2354eaa66e03d885b250
Volodymyr Kopynets <v.kopynets@enkonix.com>
2022-12-01 10:53:05 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="layout">
+      <div class="container">
+        <Nuxt />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style>
+html {
+  background: #ccc;
+  height: 100%;
+  min-height: 100vh;
+}
+</style>
